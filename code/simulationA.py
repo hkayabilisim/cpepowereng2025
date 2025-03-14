eefb6ce68600f693faca07c0565f3d9aa73e96ad
Muhammed Turhan ÇAKIR <muhammed.mtc@gmail.com>
2025-03-14 13:13:13 +0300

    SimulasyonA ve B yazıldı.

--- code/simulationA.py
+++ code/simulationA.py
@@ -31,7 +31,54 @@
 # 6. Keep relative path convention in DSS files.
 #
 print('Simulation A started!')
-#
-# The simulation code goes in between.
-#
+import opendssdirect as dss
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+DIRECTORY_CODE = os.path.dirname(os.path.abspath(__file__))
+DIRECTORY_CPEPOWERENG2025 = os.path.abspath(os.path.join(DIRECTORY_CODE, ".."))
+DIRECTORY_DSS = os.path.join(DIRECTORY_CODE, "..", "data", "IEEE123Master_Loads.dss")
+DIRECTORY_RESULTDIR = os.path.join(DIRECTORY_CPEPOWERENG2025, 'result')
+DIRECTORY_RESULT = os.path.join(DIRECTORY_RESULTDIR, 'NoPV_Taps5_Hour12.pdf')
+
+os.makedirs(DIRECTORY_RESULTDIR, exist_ok=True)
+
+print(DIRECTORY_CODE)
+dss.Text.Command('Clear')
+dss.Text.Command(f'Compile {DIRECTORY_DSS}')
+dss.Text.Command('set maxcontroliter = 10000')
+dss.Text.Command('set mode = daily')
+dss.Text.Command('set number = 1')
+dss.Text.Command('set step = 1h')
+
+
+taps = np.int32(np.ones(dss.RegControls.Count())*5)
+
+dss.RegControls.First()
+for i in range(dss.RegControls.Count()):
+    dss.RegControls.MaxTapChange(0)
+    dss.RegControls.TapNumber(taps[i])
+    dss.RegControls.Next()
+
+
+
+dss.Text.Command('set hour = 12')
+dss.Solution.LoadMult(1)
+dss.Text.Command('batchedit PVSystem..* enabled=false')
+dss.Text.Command('batchedit InvControl..* enabled=false')
+
+dss.Solution.Solve()
+
+plt.plot(dss.Circuit.AllNodeVmagPUByPhase(1), label='Phase A', marker='o')
+plt.plot(dss.Circuit.AllNodeVmagPUByPhase(2), label='Phase B', marker='o')
+plt.plot(dss.Circuit.AllNodeVmagPUByPhase(3), label='Phase C', marker='o')
+
+plt.ylabel('Voltage magnitude (p.u.)')
+plt.xlabel('Node Number')
+plt.legend()
+plt.grid()
+
+
+plt.savefig(DIRECTORY_RESULT, format="pdf", dpi=300, bbox_inches="tight")
 print('Simulation A ended!')
